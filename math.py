dc1f8762ff0ce115d887091c071639e28f6b373b
Mutesi-Bridget <bridgettemutesi@gmail.com>
2023-03-31 15:54:11 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(nam1,nam2):
+    answer = nam1-nam2
+    return answer 
+
+def divide(x,y):
+    answer = x/y
+    return answer
+
+def multiply(t,z):
+    answer = t*z
+    return answer  
+
+def reminder(q,w):
+    answer = q%w
+    return answer      
